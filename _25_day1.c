7b47951f08304ac40cc35d10fc402dc75c0757c6
sonali1127 <125128826+sonali1127@users.noreply.github.com>
2023-03-13 20:53:39 +0530

    Add files via upload

---
+++ _25_day1.c
@@ -0,0 +1,14 @@
+//write a program to print sum of all the array elements
+#include<stdio.h>
+
+int main(){
+    int i,n,sum=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    sum+=a[i];
+    printf("%d",sum);
+    return 0;
+}
