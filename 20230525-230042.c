bb1b21220942eb00ffdf0739993c71e501c6f9bd
sonali1127 <125128826+sonali1127@users.noreply.github.com>
2023-05-25 23:01:56 +0530

    Add files via upload

---
+++ 20230525-230042.c
@@ -0,0 +1,20 @@
+//wap to reverse the given string and print both strings 
+#include<stdio.h>
+
+int main()
+{
+    char str[100];
+    int ch,i=0;
+    gets(str);
+    printf("given string : ");
+    while(str[i]!='\0'){
+    printf("%c",str[i]);
+    i++;
+    }
+    printf("\nreverse of given string : ");
+    while(i>=0){
+    printf("%c",str[i]);
+    i--;
+    }
+    return 0;
+}
